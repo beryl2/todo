40e273f3b379bc60e4f94cac4c9dff40156c222c
beryl2 <berylakinyi150@gmail.com>
2022-07-02 05:56:14 +0300

    update forms

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from asyncio import tasks
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
@@ -12,5 +12,17 @@ def index(request):
 
     form = TaskForm()
 
+    if request.method =='POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():    #this will save it to the database
+            form.save()
+        return redirect('/')    
+
+
+
     context = {'tasks':tasks, 'form':form}
     return render(request, 'tasks/list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id = pk) #this view will bwe dynamic and have a dynamic url path 
+    return render(request, 'tasks/update_task.html')
